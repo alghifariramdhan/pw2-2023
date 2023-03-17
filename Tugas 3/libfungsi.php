edb625d0f53071b970c7a85de414c5bb8db62867
alghifariramdhan <114472423+alghifariramdhan@users.noreply.github.com>
2023-03-18 00:48:18 +0700

    Add files via upload

---
+++ Tugas 3/libfungsi.php
@@ -0,0 +1,43 @@
+<?php
+/**
+* Task 4 - Function
+* function kelulusan
+* function grade
+* function predikat
+*/
+
+	function kelulusan(){
+		/**
+		* Function kelulusan, jika:
+		* total nilai lebih dari 55 maka lulus
+		* total nilai kurang dari 55 maka tidak lulus
+		*/
+	}
+	
+	function grade() {
+	  /**
+		* Function grade, jika:
+		* total nilai <= 100 maka A
+		* total nilai <= 84 maka B
+		* total nilai <= 69 maka C
+		* total nilai <= 55 maka D
+		* total nilai <= 35 maka E
+		* selain itu maka I
+		*/ 
+	}
+	
+	function predikat(){
+		/**
+		* Function predikat, jika:
+		* grade A maka Sangat Memuaskan
+		* grade B maka Memuaskan
+		* grade C maka Cukup
+		* grade D maka Kurang
+		* grade E maka Sangat Kurang
+		* selain itu maka Tidak Ada
+		*
+		* Tips: Gunakan switch untuk mempermudah 
+		* melakukan pengecekan kondisi berdasarkan function grade
+		*/ 
+	}
+?>
