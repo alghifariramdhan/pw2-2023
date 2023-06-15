a2be9b3d62670fb695b244044da0ddf20a7b5515
alghifariramdhan <alghifariramdhan4@gmail.com>
2023-06-16 00:13:51 +0700

    pw

---
+++ README.md
@@ -0,0 +1,18 @@
+# Task
+Lanjutkan halaman Genres dan Reviews:
+1. Buat fitur Edit untuk masing-masing halaman
+2. Gunakan CSRF & Validation dengan baik
+3. Tampilkan data dari database ke view (Eloquent)
+4. Jangan lupa gunakan relasi
+5. Terapkan template engine & folder structure pada landing page
+6. Rapihkan halaman web
+
+#### Halaman Genres:
+
+> Berisi data no, genre dan deskripsi
+
+#### Halaman Reviews:
+
+> Berisi data film, user, rating, review dan tanggal review
+
+**Note**: Detailnya cek pada modul [s.id/notionPW2](https://s.id/notionPW2).
---
+++ movie-app/routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MovieController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [HomeController::class, 'index']);
+
+Route::resource('/movies', MovieController::class);
+
+Route::get('/genres', function () {
+    return view('genres/index');
+});
+
+Route::get('/reviews', function () {
+    return view('reviews/index');
+});
+
+Route::get('/users', function () {
+    return view('users/index');
+});
